0be30047ebf8e2d9c6ef21985a2285c05284fd99
Huy <quocthaihuyphhan@gmail.com>
2025-12-01 22:20:06 +0700

    chinh sua them pushhaha

--- new.py
+++
@@ -1,6 +0,0 @@
-
-print("Đây là chương tình dùng để thao tác git và github")
-print("Thêm dòng mới để kiểm tra commit")
-print("con o day chinh het lại lyn")
-print("dong nay de pull tu may ve")
-print("o day sua dong nay lai mot chut")
