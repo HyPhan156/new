8ff0bfb4d9eefc0e0c7488b7086919a1295f7690
HyPhan156 <Quocthaihuyphan@gmail.com>
2025-12-01 21:36:56 +0700

    Update new.py
    chinh ten github

--- new.py
+++ new.py
@@ -1,6 +1,6 @@
 
 print("Đây là chương tình dùng để thao tác git và github")
 print("Thêm dòng mới để kiểm tra commit")
-print("Dòng này được thêm vào sau khi tạo nhánh mới hahaha")
+print("con o day chinh het lại lyn")
 print("dong nay de pull tu may ve")
-print("Dòng này được thêm vào sau khi tạo nhánh mới lần 2 hehe")
+print("o day sua dong nay lai mot chut")
