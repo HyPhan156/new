31a1e18cbc2870d7b9f1259c50f9904ad4c0b1c2
HyPhan156 <Quocthaihuyphan@gmail.com>
2025-12-01 21:08:35 +0700

    Modify print statements in new.py
    Updated print statements to include additional messages.

--- new.py
+++ new.py
@@ -1,4 +1,5 @@
 
 print("Đây là chương tình dùng để thao tác git và github")
 print("Thêm dòng mới để kiểm tra commit")
-print("Dòng này được thêm vào sau khi tạo nhánh mới")
+print("Dòng này được thêm vào sau khi tạo nhánh mới hahaha")
+print("dong nay de pull tu may ve")
